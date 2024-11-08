61287d079b3423b6157225e5a6b42591d3b2e863
ChenXu0704 <chenxu0704go@gmail.com>
2024-11-08 14:08:23 -0500

    Added pushshift part for processing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from omegaconf import DictConfig, OmegaConf
 import hydra
 import pandas as pd
-from preprocessing.price import price_preprocessing
 import os 
+from preprocessing.price import price_preprocessing
+from reddit_post_scrape import reddit_post_scrape
+
 def main() -> None:
     config = OmegaConf.load("./config.yaml")
     crypto_list = config.crypto_list
@@ -10,12 +12,18 @@ def main() -> None:
     crypto_data_format = config.data.crypto_price.indata_format
     crypto_data_replace = config.data.crypto_price.replace_pattern
     main_dir = os.getcwd()
+    scrape_helper = config.data.reddit_post.scrape_method
     for crypto in crypto_list:
         crypto_file = main_dir + crypto_data_dir + crypto_data_format
         crypto_file = crypto_file.replace(crypto_data_replace, crypto)
         price_preprocessing.crypto_price_preprocessing(crypto_file)
-        
-        break
+        if scrape_helper == 'pushshift':
+            reddit_post_scrape.scrape_with_pushshift()
+        elif scrape_helper == 'selenium':
+            continue
+        else:
+            print("Please chosse from 'pushshift' and 'selenium' from scrape_method in config file")
+            return
 
         
 
